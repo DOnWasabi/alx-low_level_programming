eb23a753143f30e5fad536f23a85cd9f1dd4d8c4
DOnWasabi <debgu2000@gmail.com>
2023-02-16 11:04:50 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	        Return (0);
+	return (0);
 }
